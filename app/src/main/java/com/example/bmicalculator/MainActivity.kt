07fa1a18252a159557f80e04f2881363984f18d5
leejinseok <sonaky47@gmail.com>
2023-09-06 08:28:58 +0900

    비만도 계산기 scaffold

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -3,41 +3,46 @@ package com.example.bmicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.bmicalculator.ui.theme.BmiCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BmiCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            HomeScreen()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BmiCalculatorTheme {
-        Greeting("Android")
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "비만도 계산기") })
+        },
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text(text = "키")
+                }
+            )
+        }
     }
 }
